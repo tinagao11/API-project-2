4fd7fbc8bc2bf693f9a491149efbc2427dff179b
Jiamin Gao <tinabb1102@gmail.com>
2024-03-03 21:17:11 -0800

    review image done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,7 +11,8 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
 const bookingRouter = require('./bookings.js');
-const spotimageRouter = require('./spot-images.js')
+const spotimageRouter = require('./spot-images.js');
+const reviewImageRouter = require('./review-image.js')
 
 //test
 router.get('/set-token-cookie', async (_req, res) => {
@@ -78,7 +79,9 @@ router.use('/reviews', reviewRouter);
 
 router.use('/bookings', bookingRouter);
 
-router.use('/spot-images', spotimageRouter)
+router.use('/spot-images', spotimageRouter);
+
+router.use('/review-images',reviewImageRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/review-image.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { Spot, SpotImage, User, Review, ReviewImage, Booking } = require('../../db/models');
+const { requireAuth, setTokenCookie, restoreUser } = require('../../utils/auth')
+const { handleValidationErrors } = require('../../utils/validation')
+const { Op } = require('sequelize');
+const { check,query } = require('express-validator');
+
+const router = express.Router();
+
+//Delete a Review Image
+router.delete('/:reviewImageId', requireAuth, async(req, res)=>{
+  const {reviewImageId}= req.params;
+
+  let reviewImage = await ReviewImage.findByPk(reviewImageId);
+
+  if(!reviewImage){
+    return res.status(404).json({
+      message: `Review Image couldn't be found`
+  })
+  }
+
+  let review = await Review.findByPk(reviewImage.reviewId)
+
+  if(review.userId !== req.user.id){
+      return res.status(403).json({message:'image must belong to the current user'})
+  }
+
+  await reviewImage.destroy();
+
+  return res.status(200).json({ message: 'Successfully deleted' })
+
+})
+
+
+
+module.exports = router;
